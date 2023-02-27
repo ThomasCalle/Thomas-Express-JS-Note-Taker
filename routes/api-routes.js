f754d7a4b6c5f5f1f97a52293a0762207b4dc889
Thomas Calle <thos.calle@gmail.com>
2023-02-27 14:53:55 -0500

    update

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,17 +2,18 @@ const router = require('express').Router();
 const fs = require ("fs");
 // We could also use the path to write a realtive path in readFileSync
 
-router.get('/api/notes', (req, res) => {
-const dbJson = JSON.parse(fs.readFileSync("db/db.json","utf8"))
+router.get('/api/notes', async (req, res) => {
+const dbJson = await JSON.parse(fs.readFileSync("db/db.json","utf8"))
 res.json(dbJson)
 });
 
 router.post('/api/notes', (req, res) => {
 const dbJson = JSON.parse(fs.readFileSync("db/db.json","utf8"))
 dbJson.push(req.body)
-fs.writeFileSync("db/db.json",JSON.stringify(dbJson))
+fs.writeFileSync("db/db.json",JSON.stringify(dbJson));
 res.json(dbJson)
 });
 
 module.exports = router; 
+
 
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,11 +7,6 @@ router.get('/', (req, res) => {
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
-  // Your code
 });
 
-router.get ("*", (requires) =>{ 
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-    });
-    
 module.exports = router;
